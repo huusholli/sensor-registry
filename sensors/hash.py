c835f3f3b1cece4d63a8312a942b8a122dfb7543
tarvainen <tarvainen.atte@gmail.com>
2020-08-27 08:03:09 +0000

    Randomize hash generation

--- sensors/hash.py
+++ sensors/hash.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from hashids import Hashids
+from random import randint
 
 def create_hash():
   timestamp = int(datetime.now().timestamp())
-  return Hashids().encode(timestamp)
+  return Hashids(randint(0, 1000)).encode(timestamp)
